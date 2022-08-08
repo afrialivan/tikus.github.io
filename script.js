b56ad2334daf20a065dd583880e1d2aa972ef307
Afrial Ivan <afrialivan0@gmail.com>
2022-08-08 20:05:12 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const tanah = document.querySelectorAll('.tanah')
+const tikus = document.querySelectorAll('.tikus')
+const papanskor = document.querySelector('.papan-skor')
+
+
+let tanahsebelumnya
+let selesai
+let skor
+
+function randomTanah(tanah){
+  const t = Math.floor(Math.random() * tanah.length)
+  const tRandom = tanah[t]
+
+  if(tRandom == tanahsebelumnya){
+    randomTanah()
+  }
+
+  tanahsebelumnya = tRandom
+
+  return tRandom
+}
+
+function randomWaktu(min, max){
+  return Math.round(Math.random() * (max - min) + min)
+}
+
+function munculTikus()
+{
+  const tRandom = randomTanah(tanah)
+  const wRandom = randomWaktu(500,1000)
+
+  tRandom.classList.add('muncul')
+  
+  
+  setTimeout(() => {
+    tRandom.classList.remove('muncul')
+    if(!selesai){
+      munculTikus()
+    }
+  }, wRandom);
+}
+
+function mulai(){
+  selesai = false
+  skor = 0
+  papanskor.textContent = skor
+
+  munculTikus()
+  setTimeout(() => {
+    selesai = true
+  }, 50000);
+}
+
+function pukul(){
+ skor++
+ papanskor.textContent = skor
+ this.parentNode.classList.remove('muncul')
+ munculTikus()
+}
+
+tikus.forEach(t => {
+  t.addEventListener('click', pukul)
+});
